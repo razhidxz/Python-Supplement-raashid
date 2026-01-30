322e6804442c930b4b5124a569b621fc915c006d
razhidxz <muhammadraashid.m_25ec@kgkite.ac.in>
2026-01-30 20:54:25 +0530

    changes done

--- problem_25.py
+++ problem_25.py
@@ -1,11 +1,9 @@
 # Problem 25: Find GCD of two numbers
 # Find and fix the error
 
-def gcd(a, b):
-    while b != 0:
-        temp = b
-        b = a % b
-        a = temp
-    return a
+import math
+
+def gcd(a,b):
+    return math.gcd(a, b)
 
 print(f"GCD of 48 and 18: {gcd(48, 18)}")
