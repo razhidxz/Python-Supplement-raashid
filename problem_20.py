7f89fd56b7a718ba19146ed2ad9e3a64441c6e6c
razhidxz <muhammadraashid.m_25ec@kgkite.ac.in>
2026-01-30 15:12:02 +0530

    changes done

--- problem_20.py
+++ problem_20.py
@@ -3,8 +3,5 @@
 
 list1 = [1, 2, 3, 4, 5]
 list2 = [4, 5, 6, 7, 8]
-common = []
-for item in list1:
-    if item in list2:
-        common.append(item)
+common = list(set(list1) & set(list2))
 print(f"Common elements: {common}")
--- problem_21.py
+++ problem_21.py
@@ -2,10 +2,7 @@
 # Find and fix the error
 
 def is_sorted(lst):
-    for i in range(len(lst) - 1):
-        if lst[i] > lst[i + 1]:
-            return False
-    return True
+    return all(lst[i] > lst[i + 1] for i in range(len(lst) - 1))
 
 numbers = [1, 2, 3, 4, 5]
 print(f"Is sorted: {is_sorted(numbers)}")
